f1de1eb5053e9335c469a6ba453bef892b234b57
Angelina Bylitskaya <angelina.bylitskaya@hiqo-solutions.com>
2025-01-28 18:22:12 +0300

    Add tsconfigpath plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@services': path.resolve(__dirname, 'src/services'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
